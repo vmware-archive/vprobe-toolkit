8211b58f4d95361b818c77b0feb7a0547a5ab3a4
Ricardo E. Gonzalez <ricardog@vmware.com>
2011-04-01 16:44:48 -0700

    -- Updated for ocaml compiler.

--- install.sh
+++ install.sh
@@ -8,11 +8,10 @@ function die {
 dir=$(pwd)
 emmettdir=$(pwd)/emmett
 emmettbin=$emmettdir/emmett
-
-ghc --version              || die "could not invoke ghc"
-(cd $emmettdir; make )     || die "could not build emmett"
-[ -x $emmettdir/emmett   ] || die "could not find emmett binary in $emmettbin"
-cp $emmettdir/emmett bin/  || die "could not copy $emmettbin to bin"
+emmettbuild=$emmettdir/build
+(cd $emmettdir && make)     || die "could not build emmett"
+[ -x $emmettbuild/emmett ]  || die "could not find emmett binary in $emmettbin"
+cp $emmettbuild/emmett bin/ || die "could not copy $emmettbin to bin"
 
 echo \
 "Add $dir/bin to your PATH environment variable, e.g.,
